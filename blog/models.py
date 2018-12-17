8570dce417555aa0ac3164779e6084683fbf58b8
Artur <art.yumaev@gmail.com>
2018-12-17 04:20:38 +0300

    update

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 
+# тут модели
+
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
